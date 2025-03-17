7278ff53fdb17e5daa1b20f0cb1a3c0c01f86baf
UmbraFlare-code <umaxuel@gmail.com>
2025-03-17 15:17:48 -0500

    commit actualizacion vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ACM/', // Correct for GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false, // Changed to false to reduce file size
-  },
 })
